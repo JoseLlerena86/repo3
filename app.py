9ac68787756418d235c0656e07ce3715a53856e3
JoseLlerena86 <josellerena86@gmail.com>
2024-01-10 01:20:20 -0300

    call function

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+def myFunc(*variableArgs):
+    print(variableArgs[0])
 myArray =[i for i in range(15)]
 print(myArray)
 for f in myArray:
     print(f)
     if f%2!=0:
         print("none\n")
+myFunc("Hello")
